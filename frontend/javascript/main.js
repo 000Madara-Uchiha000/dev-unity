28755a16f3be88450e4e9a63762ca31e5fc8f94b
Abdulaziz <you@example.com>
2024-09-17 18:34:46 +0500

    another commit

--- frontend/javascript/main.js
+++ frontend/javascript/main.js
@@ -18,3 +18,22 @@ var typed = new Typed(".auto-type", {
   loopCount: 1,
   showCursor: true, 
 })
+
+window.onload = function(){
+  let loc = localStorage.getItem("token"); 
+  axios({
+    url: "http://localhost:8080/api/check/role/user",
+    method: "GET",
+    headers: {
+    "Content-Type": "application/json",
+    Authorization: `${loc}`,
+    },
+}).catch((res)=>{
+console.log(res.response.status)
+if(res.response.status == 401){
+  console.log(res.response);
+}
+}).then((res)=>{
+
+})
+} 
